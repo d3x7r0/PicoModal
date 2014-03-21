7718a35b26529a40343a7a956c28b7b7b4f4ff22
Luis Nabais <developer3@impresa.pt>
2014-03-21 19:13:45 +0000

    Added optional AMD loading

--- src/picoModal.js
+++ src/picoModal.js
@@ -23,208 +23,219 @@
 /**
  * A self-contained modal library
  */
-window.picoModal = (function(window, document) {
-    "use strict";
+!function (name, definition) {
 
-    // Generates observable objects that can be watched and triggered
-    var observable = function() {
-        var callbacks = [];
-        return {
-            watch: function(callback) {
-                callbacks.push(callback);
-            },
-            trigger: function() {
-                for (var i = 0; i < callbacks.length; i++) {
-                    window.setTimeout(callbacks[i], 1);
-                }
-            }
-        };
-    };
+    if (typeof module != 'undefined') module.exports = definition()
+    else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
+    else this[name] = definition()
 
-    // A small interface for creating and managing a dom element
-    var make = function( parent ) {
+}('picoModal', function () {
 
-        var elem = document.createElement('div');
-        (parent || document.body).appendChild(elem);
+    var picoModal = (function(window, document) {
+        "use strict";
 
-        var iface = {
-
-            elem: elem,
-
-            // Creates a child of this node
-            child: function () {
-                return make(elem);
-            },
-
-            // Applies a set of styles to an element
-            stylize: function(styles) {
-                styles = styles || {};
-
-                if ( typeof styles.opacity !== "undefined" ) {
-                    styles.filter =
-                        "alpha(opacity=" + (styles.opacity * 100) + ")";
-                }
-
-                for (var prop in styles) {
-                    if (styles.hasOwnProperty(prop)) {
-                        elem.style[prop] = styles[prop];
-                    }
-                }
-
-                return iface;
-            },
-
-            // Adds a class name
-            clazz: function (clazz) {
-                elem.className += clazz;
-                return iface;
-            },
-
-            // Sets the HTML
-            html: function (content) {
-                elem.innerHTML = content;
-                return iface;
-            },
-
-            // Returns the width of this element
-            getWidth: function () {
-                return elem.clientWidth;
-            },
-
-            // Adds a click handler to this element
-            onClick: function(callback) {
-                if (elem.attachEvent) {
-                    elem.attachEvent('onclick', callback);
-                }
-                else {
-                    elem.addEventListener('click', callback);
-                }
-
-                return iface;
-            },
-
-            // Removes this element from the DOM
-            destroy: function() {
-                document.body.removeChild(elem);
-                return iface;
-            }
-
+        // Generates observable objects that can be watched and triggered
+        var observable = function() {
+            var callbacks = [];
+            return {
+                watch: function(callback) {
+                           callbacks.push(callback);
+                       },
+        trigger: function() {
+                     for (var i = 0; i < callbacks.length; i++) {
+                         window.setTimeout(callbacks[i], 1);
+                     }
+                 }
+            };
         };
 
-        return iface;
-    };
+        // A small interface for creating and managing a dom element
+        var make = function( parent ) {
 
-    // An interface for generating the grey-out effect
-    var overlay = function( getOption ) {
+            var elem = document.createElement('div');
+            (parent || document.body).appendChild(elem);
 
-        // The registered on click events
-        var clickCallbacks = observable();
+            var iface = {
 
-        // The overlay element
-        var elem = make()
-            .clazz("pico-overlay")
-            .stylize({
-                display: "block",
-                position: "fixed",
-                top: "0px",
-                left: "0px",
-                height: "100%",
-                width: "100%",
-                zIndex: 10000
-            })
+                elem: elem,
+
+                // Creates a child of this node
+                child: function () {
+                    return make(elem);
+                },
+
+                // Applies a set of styles to an element
+                stylize: function(styles) {
+                             styles = styles || {};
+
+                             if ( typeof styles.opacity !== "undefined" ) {
+                                 styles.filter =
+                                     "alpha(opacity=" + (styles.opacity * 100) + ")";
+                             }
+
+                             for (var prop in styles) {
+                                 if (styles.hasOwnProperty(prop)) {
+                                     elem.style[prop] = styles[prop];
+                                 }
+                             }
+
+                             return iface;
+                         },
+
+                // Adds a class name
+                clazz: function (clazz) {
+                           elem.className += clazz;
+                           return iface;
+                       },
+
+                // Sets the HTML
+                html: function (content) {
+                          elem.innerHTML = content;
+                          return iface;
+                      },
+
+                // Returns the width of this element
+                getWidth: function () {
+                              return elem.clientWidth;
+                          },
+
+                // Adds a click handler to this element
+                onClick: function(callback) {
+                             if (elem.attachEvent) {
+                                 elem.attachEvent('onclick', callback);
+                             }
+                             else {
+                                 elem.addEventListener('click', callback);
+                             }
+
+                             return iface;
+                         },
+
+                // Removes this element from the DOM
+                destroy: function() {
+                             document.body.removeChild(elem);
+                             return iface;
+                         }
+
+            };
+
+            return iface;
+        };
+
+        // An interface for generating the grey-out effect
+        var overlay = function( getOption ) {
+
+            // The registered on click events
+            var clickCallbacks = observable();
+
+            // The overlay element
+            var elem = make()
+                .clazz("pico-overlay")
+                .stylize({
+                    display: "block",
+                    position: "fixed",
+                    top: "0px",
+                    left: "0px",
+                    height: "100%",
+                    width: "100%",
+                    zIndex: 10000
+                })
             .stylize( getOption('overlayStyles', {
                 opacity: 0.5,
                 background: "#000"
             }) )
             .onClick(clickCallbacks.trigger);
 
-        return {
-            elem: elem.elem,
-            destroy: elem.destroy,
-            onClick: clickCallbacks.watch
+            return {
+                elem: elem.elem,
+                    destroy: elem.destroy,
+                    onClick: clickCallbacks.watch
+            };
         };
-    };
 
-    // A function for easily displaying a modal with the given content
-    return function(options) {
+        // A function for easily displaying a modal with the given content
+        return function(options) {
 
-        if ( typeof options === "string" ) {
-            options = { content: options };
-        }
+            if ( typeof options === "string" ) {
+                options = { content: options };
+            }
 
-        // Returns a named option if it has been explicitly defined. Otherwise,
-        // it returns the given default value
-        function getOption ( opt, defaultValue ) {
-            return options[opt] === void(0) ? defaultValue : options[opt];
-        }
+            // Returns a named option if it has been explicitly defined. Otherwise,
+            // it returns the given default value
+            function getOption ( opt, defaultValue ) {
+                return options[opt] === void(0) ? defaultValue : options[opt];
+            }
 
-        var shadow = overlay( getOption );
+            var shadow = overlay( getOption );
 
-        var closeCallbacks = observable();
+            var closeCallbacks = observable();
 
-        var elem = make()
-            .clazz("pico-content")
-            .stylize({
-                display: 'block',
-                position: 'fixed',
-                zIndex: 10001,
-                left: "50%",
-                top: "50px"
-            })
+            var elem = make()
+                .clazz("pico-content")
+                .stylize({
+                    display: 'block',
+                    position: 'fixed',
+                    zIndex: 10001,
+                    left: "50%",
+                    top: "50px"
+                })
             .html(options.content);
 
-        var width = getOption('width', elem.getWidth());
+            var width = getOption('width', elem.getWidth());
 
-        elem
-            .stylize({
-                width: width + "px",
-                margin: "0 0 0 " + (-(width / 2) + "px")
-            })
+            elem
+                .stylize({
+                    width: width + "px",
+                    margin: "0 0 0 " + (-(width / 2) + "px")
+                })
             .stylize( getOption('modalStyles', {
                 backgroundColor: "white",
                 padding: "20px",
                 borderRadius: "5px"
             }) );
 
-        var close = function () {
-            closeCallbacks.trigger();
-            shadow.destroy();
-            elem.destroy();
-        };
+            var close = function () {
+                closeCallbacks.trigger();
+                shadow.destroy();
+                elem.destroy();
+            };
 
-        if ( getOption('overlayClose', true) ) {
-            shadow.onClick(close);
-        }
+            if ( getOption('overlayClose', true) ) {
+                shadow.onClick(close);
+            }
 
-        var closeButton;
-        if ( getOption('closeButton', true) ) {
-            closeButton = elem.child()
-                .html( getOption('closeHtml', "&#xD7;") )
-                .clazz("pico-close")
-                .stylize( getOption('closeStyles', {
-                    borderRadius: "2px",
-                    cursor: "pointer",
-                    height: "15px",
-                    width: "15px",
-                    position: "absolute",
-                    top: "5px",
-                    right: "5px",
-                    fontSize: "16px",
-                    textAlign: "center",
-                    lineHeight: "15px",
-                    background: "#CCC"
-                }) )
+            var closeButton;
+            if ( getOption('closeButton', true) ) {
+                closeButton = elem.child()
+                    .html( getOption('closeHtml', "&#xD7;") )
+                    .clazz("pico-close")
+                    .stylize( getOption('closeStyles', {
+                        borderRadius: "2px",
+                        cursor: "pointer",
+                        height: "15px",
+                        width: "15px",
+                        position: "absolute",
+                        top: "5px",
+                        right: "5px",
+                        fontSize: "16px",
+                        textAlign: "center",
+                        lineHeight: "15px",
+                        background: "#CCC"
+                    }) )
                 .onClick(close);
-        }
+            }
 
-        return {
-            modalElem: elem.elem,
-            closeElem: closeButton ? closeButton.elem : null,
-            overlayElem: shadow.elem,
-            close: close,
-            onClose: closeCallbacks.watch
+            return {
+                modalElem: elem.elem,
+                closeElem: closeButton ? closeButton.elem : null,
+                overlayElem: shadow.elem,
+                close: close,
+                onClose: closeCallbacks.watch
+            };
         };
-    };
 
-}(window, document));
+    }(window, document));
 
+    return picoModal;
+
+});
